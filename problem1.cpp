e86d8234af60a8e38db48280d348082e249397e7
Ozodbek87 <o.sultonov@newuu.uz>
2024-12-06 13:50:34 +0500

    Add files via upload

--- problem1.cpp
+++ problem1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 bool isPrime(int num) {
-    if (num <= 1) {
+    if (num <= 1 && num>=10000) {
         return false;
     }
     for (int i = 2; i <=num; ++i) {
